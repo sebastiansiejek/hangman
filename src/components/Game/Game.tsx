d5a22f3be765f7a5d2320d44d488e801de0776e4
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-10-18 20:42:45 +0200

    feat: add Game styled container

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 import AddChar from '../AddChar'
 import Chars from '../Chars'
 import CorrectWord from '../CorrectWord'
 import HangMan from '../HangMan'
 
+const StyledContainer = styled.div`
+  display: grid;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
+
 const Game: React.FC = () => (
-  <div
-    className="App"
-    style={{
-      display: 'grid',
-      gap: '2rem',
-      justifyContent: 'center'
-    }}
-  >
+  <StyledContainer>
+    <h1>HANGMAN</h1>
     <Chars />
     <AddChar />
     <CorrectWord />
     <HangMan />
-  </div>
+  </StyledContainer>
 )
 
 export default Game
